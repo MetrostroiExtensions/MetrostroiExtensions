55315aa88a6b0975a48c45ab774c0b526b380619
fedotdev <fedorbol.mos@gmail.com>
2025-10-04 13:07:10 +0300

    add langs for custom lvz

--- lua/metrostroi_data/languages/ru_ext_main.lua
+++ lua/metrostroi_data/languages/ru_ext_main.lua
@@ -9,7 +9,7 @@ Entities.gmod_subway_81-717_mvm_custom.Spawner.SalonLampType.Type1 = ЛПВ-02
 Entities.gmod_subway_81-717_mvm_custom.Spawner.SalonLampType.Type2 = ЛЛВ-01
 
 # salon seats
-Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.Name = Сидения в салоне
+Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.Name = Сиденья в салоне
 Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.Random = @[Common.Spawner.Random]
 Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.Old = @[Common.Spawner.Old]
 Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.New = @[Common.Spawner.New]
@@ -20,4 +20,43 @@ Entities.gmod_subway_81-717_mvm_custom.Spawner.Section.Cabine = Кабина
 Entities.gmod_subway_81-717_mvm_custom.Spawner.Section.Equipment = Оборудование
 Entities.gmod_subway_81-717_mvm_custom.Spawner.Section.Console = Пульт
 Entities.gmod_subway_81-717_mvm_custom.Spawner.Section.Interior = Салон
+
+# Custom LVZ SPB
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Type.Name = @[Spawner.717.Type]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Type.1 = @[Spawner.717.Line2]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Type.2 = @[Spawner.717.Line4]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Type.3 = @[Spawner.717.Line5]
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SpawnMode.Name = @[Common.Spawner.SpawnMode]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SpawnMode.1 = @[Common.Spawner.SpawnMode.Deadlock]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SpawnMode.2 = @[Common.Spawner.SpawnMode.Full]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SpawnMode.3 = @[Common.Spawner.SpawnMode.NightDeadlock]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SpawnMode.4 = @[Common.Spawner.SpawnMode.Depot]
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.CabTexture.Name = @[Common.Spawner.CabTexture]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Scheme.Name = @[Common.Spawner.Scheme]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Scheme.Random = @[Common.Spawner.Random]
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Texture.Name = @[Common.Spawner.Texture]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Texture.Random = @[Common.Spawner.Random]
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.PassTexture.Name = @[Common.Spawner.PassTexture]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.PassTexture.Random = @[Common.Spawner.Random]
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.WagonType.Name = @[Spawner.717.Type]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.WagonType.Random = @[Common.Spawner.Random]
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SalonLampType.Name = @[Spawner.717.LampType]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SalonLampType.Random = @[Common.Spawner.Random]
+
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SeatType.Name = @[Spawner.717.SeatType] #FIXME
+Entities.gmod_subway_81-717_lvz_custom.Spawner.SeatType.Random = @[Common.Spawner.Random] #FIXME
+
+
+# sections SPB
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Section.Body = @[Entities.gmod_subway_81-717_mvm_custom.Spawner.Section.Body]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Section.Cabine = @[Entities.gmod_subway_81-717_mvm_custom.Spawner.Section.Cabine]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Section.Equipment = @[Entities.gmod_subway_81-717_mvm_custom.Spawner.Section.Equipment]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Section.Console = @[Entities.gmod_subway_81-717_mvm_custom.Spawner.Section.Console]
+Entities.gmod_subway_81-717_lvz_custom.Spawner.Section.Interior = @[Entities.gmod_subway_81-717_mvm_custom.Spawner.Section.Interior]
 ]]
